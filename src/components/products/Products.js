a6a03460e67427834481d28e7b55680ce4fc5fce
[synnaf] <f.vforsman@gmail.com>
2020-12-15 16:26:12 +0100

    init

---
+++ src/components/products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Products() {
+  return (
+    <div className="Products">
+        This is products 
+    </div>
+  );
+}
+
+export default Products;
---
+++ src/components/search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Search() {
+
+  return (
+    <div className="Search">
+        Search: 
+        <input type="text" placeholder="Search for something..." />
+    </div>
+  );
+}
+
+export default Search;
