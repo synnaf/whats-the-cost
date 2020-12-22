c437ac7e73afa325080aa153881e667f53dea363
[synnaf] <f.vforsman@gmail.com>
2020-12-22 21:43:51 +0100

    adding mobile menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,10 +1,27 @@
 import React from 'react'; 
+import './Menu.scss';  
 
 const Menu = () => {
     return (
-        <div>
-            Menu component
-        </div>
+        <nav className="main-navigation">
+            <button className="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+            <ul>
+                <li>
+                    <a>Menu</a>
+                </li>
+                <li>
+                    <a>Search</a>
+                </li>
+                <li>
+                    <a>Contact</a>
+                </li>
+            </ul>
+        </nav>
     ); 
 }
 
