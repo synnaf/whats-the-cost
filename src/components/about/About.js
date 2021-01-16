cdaa391f9f48f255200b70e3117894b4044ae088
[synnaf] <f.vforsman@gmail.com>
2021-01-16 13:36:38 +0100

    added about page info + style

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,30 +1,17 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState } from 'react';
 import Header from '../header/Header'; 
+import Info from './Info'; 
 import './About.scss'; 
 
 const About = (props) => {    
-    console.log(props); //props is 'test', same as value in page
-    let defaultValue =''; 
-    const [showNotes, setShowNotes] = useState(false); 
-    const [info, setInfo] = useReducer( 
-        (state, newState) => ({ ...state, ...newState}), 
-        defaultValue //startvärdet på vårt state   
-    );  
+    const [info, setInfo] = useState('');  
 
-    function update(e) { 
-        //detta item måste vara accordion__link med rätt html-value, annars funkar det inte 
-        let clickedItem = e.target.innerHTML; 
-        console.log('clicke ditem', clickedItem); 
-
-        setInfo({['t']: e.target.innerHTML }); //this should be toggled??
-
-        
-        animateArrow(e.target);
-    }; 
-
-    function animateArrow(el) {
-        console.log('recieve', el); 
-        el.classList.toggle('--active'); 
+    function displayInfo(e) { 
+        setInfo(e.target.innerHTML); 
+        let element = document.getElementById('submenu');
+        console.log(element); 
+        element.classList.toggle('hide'); 
+        // element.style.display = 'hidden'; 
     }; 
 
     return (
@@ -34,58 +21,31 @@ const About = (props) => {
                 <section className="page__about">
                     <div className="submenu">
                         <ul className="accordion">
-                            <li className="accordion__item" id="test">
-                                <button className="accordion__link" onClick={update}>
+                            <li className="accordion__item">
+                                <a className="accordion__link" 
+                                    onClick={(e) => displayInfo(e)}
+                                >
                                     About
-
-                                </button>
-                                <div className="icon">
-                                        <div className="arrow"></div>
-                                </div>
-                                {
-                                    info.t == 'About' ?
-                                        <div id="submenuInfo" className="submenu__info">
-                                            <span>Some info text 1. 
-                                                And some more info text. And some more info text.
-                                                And some more info text
-                                            </span>
-                                        </div>
-                                    : null
-                                }
-
+                                </a>
                             </li>
                             <li className="accordion__item">
-                                <a className="accordion__link" onClick={update}>Our values
-                                    <div className="icon">
-                                        <div className="arrow"></div>
-                                    </div>
+                                <a className="accordion__link" 
+                                    onClick={(e) => displayInfo(e)}
+                                >
+                                    The ConsuValue
                                 </a>
-                                {
-                                    info.t == 'Our values' ?
-                                        <div id="submenuInfo" className="submenu__info">
-                                            <span>Some info text 2</span>
-                                        </div>
-                                    : null
-                                }
                             </li>
                             <li className="accordion__item">
-                                <a className="accordion__link" onClick={update}>Contact
-                                <div className="icon">
-                                    <div className="arrow"></div>
-                                </div>
+                                <a className="accordion__link" 
+                                    onClick={(e) => displayInfo(e)}
+                                >
+                                    Contact
                                 </a>
-                                {
-                                    info.t == 'Contact' ? 
-                                        <div id="submenuInfo" className="submenu__info">
-                                            <span>Some info text 3</span>
-                                        </div>
-                                    : null
-                                }
                             </li>
                         </ul>
-                        {/* <button type="button" className={`btn ${props}`}>
-                            Click
-                        </button> */}
+                    </div>
+                    <div className="submenu--active hide" id="submenu">
+                        <Info info={info} />
                     </div>
                 </section>
             </section>
---
+++ src/components/about/Info.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+
+const Info = (props) => { 
+
+    console.log(props); 
+    if(props.info == 'About') {
+        // el.classList.toggle('--active');
+      
+
+        return (
+            <span>
+                This page is created as an FrontEnd Examproject.
+                The data is collected using a v.2 of a closed API to Consupedia.
+                I wanted to enhance the look of a possible product search, and potentially
+                further elaborate a price function, and filter functions.
+            </span>
+        ); 
+    } 
+    if(props.info == 'The ConsuValue') {
+        return (
+            <>
+            <span>
+                The ConsuValue is a value calculated
+               based on each products five key points:
+           </span>
+                <ul>
+                    <li>
+                        Health,
+                    </li>
+                    <li>
+                        Environment,
+                    </li>
+                    <li>
+                        Social justice,
+                    </li>
+                    <li>
+                        Quality,
+                    </li>
+                    <li>
+                        Price
+                    </li>
+                </ul>
+            <span>
+            A simple guideline is:
+                "The higher ConsuValue, the better the product".
+                Details about each products values can be found on ConsuPedia!
+            </span>
+            <a href="https://consupedia.se/about-consuvalue">Read more</a>
+        </> 
+        ); 
+    } 
+    if(props.info == 'Contact') {
+        return(
+            <span>
+                Reach out if you have any questions!
+                <ul>
+                    <li>
+                        If you have questions about the data:
+                        <a href="www.consupedia.se">Visit Consupedia!</a>
+                    </li>
+                    <li>
+                        If you have questions to the page creator:
+                        <a href="#">My email</a>
+                    </li>
+                    <li>
+                        General life questions:
+                        <a href="www.google.se">Go here</a>
+                    </li>
+                </ul>
+            </span>
+        );
+    }
+
+    return( null ); 
+}
+
+export default Info; 
