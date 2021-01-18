e5e102b6a0b46df04f8fde914f0dff66d3c597e3
[synnaf] <f.vforsman@gmail.com>
2021-01-18 14:58:37 +0100

    changed icon structur

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.scss'; 
-
+import { Instagram } from '../../assets/svg/Icons'; 
 //lägg in classNames här, så att den hämtar in dem automatiskt? 
 
 const Footer = () => {
@@ -20,7 +20,18 @@ const Footer = () => {
             <div className="copyright">
               <h6>2021</h6>
               <p>Fanny Värnbrink Forsman</p>
+              <a
+                href="instagram-com/synnaf"
+                title="Instagram"
+                target="_blank"
+                className="instagram"
+              >
+                <Instagram />
+              </a>
             </div>
+
+          
+   
         </footer>
       </>
 
